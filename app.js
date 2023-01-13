2203da57f6f940831856d87330150df5df3c5b07
Gawain <a0900492616@gmail.com>
2023-01-13 21:42:24 +0800

    0113 1st

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+
+const passport = require('passport')
+
+const session = require('express-session')
 const connectDB = require('./config/db')
 // Load config
 dotenv.config({ path: './config/config.env' })
+
+// Passport Config
+require('./config/passport')(passport)
 
 connectDB()
 
@@ -20,12 +27,25 @@ if (process.env.NODE_ENV === 'development') {
 app.engine('.hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', '.hbs')
 
+// Session
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    // cookie: { secure: true },
+}))
+
+// Passport Middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
 // Static Folder
 app.use(express.static(path.join(__dirname, 'public')))
 
 
 // Routes
 app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
 
 const PORT = process.env.PORT || 3000
 
