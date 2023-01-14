d91fff6f38cc468c4e82cce301fd2d9d1c0cc3a9
Gawain <a0900492616@gmail.com>
2023-01-14 14:38:00 +0800

    0114 5th

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const Proposal = require('../models/Proposal')
 // @desc Login/Landing page
 // @route GET / 
 router.get('/', ensureGuest, (req, res) => {
-    res.render('login', {
+        res.render('login', {
         layout: 'login',
     })
 })
@@ -15,20 +15,20 @@ router.get('/', ensureGuest, (req, res) => {
 // @desc Dashboard
 // @route GET / dashboard
 router.get('/dashboard', ensureAuth, async (req, res) => {
-
+    
     try {
         const proposals = await Proposal.find({ user: req.user.id }).lean()
-
+        
         res.render('dashboard', {
             firstName: req.user.firstName,
             proposals
         })
-
+        
     } catch (err) {
         console.error(err)
         res.render('error/500')
     }
-
+    
 })
 
 module.exports = router
--- routes/proposals.js
+++ routes/proposals.js
@@ -43,46 +43,91 @@ router.get('/', ensureAuth, async (req, res) => {
     
 })
 
+// @desc Show single story
+// @route GET /proposals/:id
+router.get('/:id', ensureAuth, async (req, res) => {
+    try {
+        let proposal = await Proposal.findById(req.params.id)
+            .populate('user')
+            .lean()
+
+        if (!proposal) {
+            return res.render('error/404')
+        }
+
+        res.render('proposals/show', {
+            proposal
+        })
+    } catch (err) {
+        console.error(err)
+        res.render('error/404')
+    }
+})
+
 // @desc Show edit page
 // @route GET /proposals/edit/:id
 router.get('/edit/:id', ensureAuth, async (req, res) => {
-    const proposal = await Proposal.findOne({
-        _id: req.params.id
-    }).lean()
+    try {
     
-    if(!proposal) {
+        const proposal = await Proposal.findOne({
+            _id: req.params.id
+        }).lean()
+        
+        if(!proposal) {
         return res.render('error/404')
-    }
-    
-    if (proposal.user != req.user.id) {
-        res.redirect('/proposals')
-    } else {
-        res.render('proposals/edit', {
-            proposal,
-        })
+        }
+        
+        if (proposal.user != req.user.id) {
+            res.redirect('/proposals')
+        } else {
+            res.render('proposals/edit', {
+                proposal,
+            })
+        }
+    } catch {
+        console.error(err)
+        return res.render('error/500')
     }
 })
 
 // @desc Update Proposal
 // @route PUT /proposals/:id
 router.put('/:id', ensureAuth, async (req, res) => {
-    let proposal = await Proposal.findById(req.params.id).lean()
-    // await is only valid in async function and the top level bodies of modules
-    if (!proposal) {
-        return res.render('error/404')
-    }
     
-    if (proposal.user != req.user.id) {
-        res.redirect('/proposals')
-    } else {
-        proposal = await Proposal.findOneAndUpdate({ _id: req.params.id }, req.body, {
-            new: true,
-            runValidators: true, 
-        })
-        res.redirect('/dashboard')
-
-    }
+    try{
+        let proposal = await Proposal.findById(req.params.id).lean()
+        // await is only valid in async function and the top level bodies of modules
+        if (!proposal) {
+            return res.render('error/404')
+        }
         
+        if (proposal.user != req.user.id) {
+            res.redirect('/proposals')
+        } else {
+            proposal = await Proposal.findOneAndUpdate({ _id: req.params.id }, req.body, {
+                new: true,
+                runValidators: true, 
+            })
+            res.redirect('/dashboard')
+        }
+    } catch(error){
+        console.error(err)
+        return res.render('error/500')
+    }
+
+        
+})
+
+// @desc Delete proposal
+// @route DELETE /proposals/:id
+router.delete('/:id', ensureAuth, async (req, res) => {
+    try {
+        await Proposal.remove({ _id: req.params.id }) 
+        res.redirect('/dashboard')
+    } catch (err) {
+        console.error(err)
+        return res.render('error/500')
+    }
 })
 
 module.exports = router
